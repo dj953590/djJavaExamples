f0d7933c557d40fa23bf8c8047ae98230287f47e
dj153590 <69596727+dj153590@users.noreply.github.com>
2022-02-16 21:23:33 -0500

    java class

---
+++ djJava/simple/djJava/printHello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package djJava;
+
+/**
+ * @author dj153
+ *
+ */
+public class printHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
